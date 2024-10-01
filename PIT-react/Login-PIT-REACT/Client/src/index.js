06c95a5b83274c7cc095329410e1af52b5bec5e6
CottaMDM13 <eduardocottappontes@gmail.com>
2024-10-01 00:10:37 -0300

    Delete PIT-react directory

--- PIT-react/Login-PIT-REACT/Client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Rotas from "./Routes/routes"
-
-
-ReactDOM.render(<Rotas />, document.getElementById("root"));
-
-
--- PIT-react/PIT1/src/pages/404/index.jsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="grid h-screen place-content-center bg-white px-4">
-      <h1 className="uppercase tracking-widest text-gray-500">
-        404 | Página não encontrada
-      </h1>
-    </div>
-  );
-}
